32af0994ba75bbbc288b33f8948adb78f4c1f32a
sunil.kumar <sunil.kumar@NTZ-CPU-105>
2026-01-15 17:14:44 +0530

    satructer

---
+++ BuildingBlock.Common/ViewModels/JobCardViewModel/JobCardVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlock.Common.ViewModels.JobCardViewModel
+{
+    public class JobCardVM
+    {
+        public string Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Status { get; set; }
+        public string DealerId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
